b30efe6ea097b77d643e20b42d91d40acd468747
Ikramullah <ikramullahzx@gmail.com>
2024-01-20 23:36:14 +0800

    cacheFarm: add .env.example and new error

--- cache_farm/src/Error.ts
+++ cache_farm/src/Error.ts
@@ -3,5 +3,11 @@ import Response from './App/Response'
 
 export default function (app: Hono): void {
   app.notFound(c => c.json(Response('Not found!'), 404))
-  app.onError((err, c) => c.json(Response('Something happened on server!', err), 500))
+
+  app.onError((err, c) => {
+    console.error(err)
+    return c.json(Response('Something happened on server!', {
+      message: err.message
+    }), 500)
+  })
 }
