549814301b5f4c9e1476545375e5ece9432f0de2
Ikramullah <ikramullahzx@gmail.com>
2024-01-20 18:33:22 +0800

    cacheFarm: fix lint

--- cache_farm/src/Controllers/Attendance.ts
+++ cache_farm/src/Controllers/Attendance.ts
@@ -1,14 +1,14 @@
 import { Context } from 'hono'
 import { TServices } from '../Service'
-import { encrypt, decrypt } from './Functions/Attendance'
+import { Encrypt, Decrypt } from './Functions/Attendance'
 import Response from '../App/Response'
 import Controller from '../App/Controller'
 
 export default class Attendance extends Controller {
   public static GenerateAttendanceCard (ctx: Context): globalThis.Response {
     const text = 'Aku ganteng'
-    const encryptTxt = encrypt(text)
-    const decryptTxt = decrypt(encryptTxt)
+    const encryptTxt = Encrypt(text)
+    const decryptTxt = Decrypt(encryptTxt)
     return ctx.json(Response('Ok', {
       text, encryptTxt, decryptTxt
     }))
--- cache_farm/src/Controllers/Functions/Attendance.ts
+++ cache_farm/src/Controllers/Functions/Attendance.ts
@@ -2,21 +2,22 @@ import { utils, ModeOfOperation, Counter } from 'aes-js'
 import 'dotenv/config'
 
 const AESKey = utils.utf8.toBytes(process.env.API_KEY ?? '').slice(0, 16)
-const AESCounter = () =>
+const AESCounter = (): ModeOfOperation.ModeOfOperationCTR =>
+  // eslint-disable-next-line new-cap
   new ModeOfOperation.ctr(
     AESKey, new Counter(
       parseInt(process.env.AES_CTR_COUNTER ?? '5')
     )
   )
 
-export const encrypt = (txt: string): string => {
-  let textBytes = utils.utf8.toBytes(txt)
-  let encrypted = AESCounter().encrypt(textBytes)
+export const Encrypt = (txt: string): string => {
+  const textBytes = utils.utf8.toBytes(txt)
+  const encrypted = AESCounter().encrypt(textBytes)
   return utils.hex.fromBytes(encrypted)
 }
 
-export const decrypt = (hex: string): string => {
-  let textBytes = utils.hex.toBytes(hex)
-  let encrypted = AESCounter().encrypt(textBytes)
+export const Decrypt = (hex: string): string => {
+  const textBytes = utils.hex.toBytes(hex)
+  const encrypted = AESCounter().encrypt(textBytes)
   return utils.utf8.fromBytes(encrypted)
 }
