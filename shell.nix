99935e6b9f11fcf2932c8e64027498368e70c4a2
Ikramullah <ikramullahzx@gmail.com>
2024-01-25 23:56:44 +0800

    testing: improving multithreading

--- shell.nix
+++ shell.nix
@@ -4,5 +4,6 @@ pkgs.mkShell rec {
   packages = with pkgs; [
     nodejs_21 php php82Packages.composer php82Extensions.pdo php82Extensions.xml
     python3 python311Packages.python-decouple python311Packages.requests
+    python311Packages.numpy
   ];
 }
--- testing/main.py
+++ testing/main.py
@@ -1,29 +1,46 @@
 """ Brutal testing chamber """
 import threading
+from multiprocessing import Process
 import requests
+import numpy as np
 from decouple import config
 
 GATEWAY_URL = config('GATEWAY_URL')
 CARD_GENERATED = config('CARD_GENERATED')
-THREAD_COUNT = config('THREAD_COUNT')
+THREAD_COUNT = int(config('THREAD_COUNT'))
 
 print('Fetching data to database')
-data = requests.post(
+database_data = requests.post(
     f"{GATEWAY_URL}/generate",
     json={"count": int(CARD_GENERATED)},
     timeout=None
 ).json().get('data')
+print(f"Get {len(database_data)} data")
 
 def send_attendance(hashed):
     """ Sending data to database """
-    req = requests.post(
+    requests.post(
         f"{GATEWAY_URL}/attendance",
         json={"data":hashed},
         timeout=None
     )
-    print(data)
 
-for i, val in enumerate(data):
-    data_hash = val.get('encrypt')
-    x = threading.Thread(target=send_attendance(data_hash))
-    x.start()
+def loop_data(data):
+    """ Spawining daemon thread for nesting multithread """
+    data_process = [Process(target=send_attendance(val)) for _, val in enumerate(data)]
+    for proc in data_process:
+        proc.start()
+    for proc in data_process:
+        proc.join()
+
+# Split data for thread
+database_data = np.array(database_data)
+database_data = np.array_split(database_data, THREAD_COUNT)
+print(f"Splitting process for {len(database_data)} data to {THREAD_COUNT} thread.")
+processes = [Process(target=loop_data(datas)) for _, datas in enumerate(database_data)]
+for r in processes:
+    r.start()
+for r in processes:
+    r.join()
+
+print('Done!')
