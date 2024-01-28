b7a987c41178b56d41dff05c97150329316f2eb7
Ikramullah <ikramullahzx@gmail.com>
2024-01-28 13:29:21 +0800

    testing: finalize add cpu and ram usage

--- testing/main.py
+++ testing/main.py
@@ -1,9 +1,10 @@
 """ Brutal testing chamber """
 from multiprocessing import Pool
 from timeit import default_timer
-from os import getpid
+from os import getpid, popen
 from datetime import datetime
 import pickle
+import subprocess
 import requests
 import numpy as np
 from decouple import config
@@ -12,6 +13,9 @@ GATEWAY_URL = config('GATEWAY_URL')
 WEBSERVER_URL = config('WEBSERVER_URL')
 CARD_GENERATED = config('CARD_GENERATED')
 THREAD_COUNT = int(config('THREAD_COUNT'))
+REDIS_SUB_PID = config('REDIS_SUB_PID')
+WEBSERVER_PID = config('WEBSERVER_PID')
+CLEAN_RUN = int(config('CLEAN_RUN'))
 
 print('Fetching data from database')
 database_data = requests.post(
@@ -20,6 +24,23 @@ database_data = requests.post(
     timeout=None
 ).json().get('data')
 print(f"Get {len(database_data)} data")
+
+def read_ram_usage(pid):
+    """ Check RAM Usage """
+    smaps = subprocess.run(
+        ['cat', f'/proc/{pid}/smaps'], check=True, capture_output=True
+    )
+    pss = subprocess.run(
+        ['grep', '-i', 'pss'], input=smaps.stdout, check=True, capture_output=True
+    )
+    awk = subprocess.run(
+        ['awk', '{Total+=$2} END {print Total}'], input=pss.stdout, check=True, capture_output=True
+    )
+    return int(awk.stdout.decode('utf-8').strip())
+
+def read_cpu_usage():
+    """ Check CPU Usage """
+    return str(round(float(popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),10))
 
 def loop_data_to_redis(data):
     """ Looping data all the way """
@@ -34,6 +55,8 @@ def loop_data_to_redis(data):
             timeout=None
         )
         result_loop.append({
+            "ram": read_ram_usage(REDIS_SUB_PID) if CLEAN_RUN == 0 else False,
+            "cpu": read_cpu_usage() if CLEAN_RUN == 0 else False,
             "start": start_time,
             "stop": default_timer() - start_time
         })
@@ -54,6 +77,8 @@ def loop_data_to_laravel(data):
             timeout=None
         )
         result_loop.append({
+            "ram": read_ram_usage(WEBSERVER_PID) if CLEAN_RUN == 0 else False,
+            "cpu": read_cpu_usage() if CLEAN_RUN == 0 else False,
             "start": start_time,
             "stop": default_timer() - start_time
         })
